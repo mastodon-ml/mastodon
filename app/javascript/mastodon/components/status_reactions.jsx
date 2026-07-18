981edf528849ca86dc36cd341b07da976e0a5f39
Mo Rijndael <git@rijndael.cc>
2026-07-18 20:55:34 +0300

    Make reactionsbar functional component

--- app/javascript/mastodon/components/status_reactions.jsx
+++ app/javascript/mastodon/components/status_reactions.jsx
@@ -1,9 +1,4 @@
-import PropTypes from 'prop-types';
-
 import classNames from 'classnames';
-
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import ImmutablePureComponent from 'react-immutable-pure-component';
 
 import { animated, useTransition } from '@react-spring/web'
 
@@ -12,19 +7,15 @@ import { reduceMotion } from '../initial_state';
 
 import { AnimatedNumber } from './animated_number';
 
-export default class StatusReactions extends ImmutablePureComponent {
+const StatusReactions = (
+    statusId,
+    reactions,
+    numVisible,
+    addReaction,
+    canReact,
+    removeReaction,
+  ) => {
 
-  static propTypes = {
-    statusId: PropTypes.string.isRequired,
-    reactions: ImmutablePropTypes.list.isRequired,
-    numVisible: PropTypes.number,
-    addReaction: PropTypes.func.isRequired,
-    canReact: PropTypes.bool.isRequired,
-    removeReaction: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { reactions, numVisible } = this.props;
     let visibleReactions = reactions
       .filter(x => x.count > 0)
       .sort((a, b) => b.count - a.count);
@@ -47,44 +38,35 @@ export default class StatusReactions extends ImmutablePureComponent {
     keys: visibleReactions.map(x => x.name),
   });
 
-    return (
-      <div className={classNames('reactions-bar', { 'reactions-bar--empty': visibleReactions.isEmpty() })}>
-        {transitions(({ scale }, reaction) => (
-          <Reaction
-            key={reaction.name}
-            statusId={this.props.statusId}
-            reaction={data}
-            style={{ transform: scale.to((s) => `scale(${s})`) }}
-            addReaction={this.props.addReaction}
-            removeReaction={this.props.removeReaction}
-            canReact={this.props.canReact}
-          />
-        )
-      )}
-      </div>
-    );
-  }
-
+  return (
+    <div className={classNames('reactions-bar', { 'reactions-bar--empty': visibleReactions.isEmpty() })}>
+      {transitions(({ scale }, reaction) => (
+        <Reaction
+          key={reaction.name}
+          statusId={statusId}
+          reaction={data}
+          style={{ transform: scale.to((s) => `scale(${s})`) }}
+          addReaction={addReaction}
+          removeReaction={removeReaction}
+          canReact={canReact}
+        />
+      )
+    )}
+    </div>
+  );
 }
 
-class Reaction extends ImmutablePureComponent {
+const Reaction = (
+    statusId,
+    reaction,
+    addReaction,
+    removeReaction,
+    canReact,
+    style,
+  ) => {
 
-  static propTypes = {
-    statusId: PropTypes.string,
-    reaction: ImmutablePropTypes.map.isRequired,
-    addReaction: PropTypes.func.isRequired,
-    removeReaction: PropTypes.func.isRequired,
-    canReact: PropTypes.bool.isRequired,
-    style: PropTypes.object,
-  };
 
-  state = {
-    hovered: false,
-  };
-
-  handleClick = () => {
-    const { reaction, statusId, addReaction, removeReaction } = this.props;
-
+  const handleClick = () => {
     if (reaction.me) {
       removeReaction(statusId, reaction.name);
     } else {
@@ -92,36 +74,25 @@ class Reaction extends ImmutablePureComponent {
     }
   }
 
-  handleMouseEnter = () => this.setState({ hovered: true })
+  const code = isUnicodeEmoji(reaction.name)
+    ? reaction.name
+    : `:${reaction.name}:`;
 
-  handleMouseLeave = () => this.setState({ hovered: false })
-
-  render() {
-    const { reaction } = this.props;
-
-    const code = isUnicodeEmoji(reaction.name)
-      ? reaction.name
-      : `:${reaction.name}:`;
-
-    return (
-      <animated.button
-        className={classNames('reactions-bar__item', { active: reaction.me })}
-        onClick={this.handleClick}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        disabled={!this.props.canReact}
-        style={this.props.style}
-      >
-        <span className='reactions-bar__item__emoji'>
-          <Emoji code={code} />
-        </span>
-        <span className='reactions-bar__item__count'>
-          <AnimatedNumber value={reaction.count} />
-        </span>
-      </animated.button>
-    );
-  }
-
+  return (
+    <animated.button
+      className={classNames('reactions-bar__item', { active: reaction.me })}
+      onClick={handleClick}
+      disabled={!canReact}
+      style={style}
+    >
+      <span className='reactions-bar__item__emoji'>
+        <Emoji code={code} />
+      </span>
+      <span className='reactions-bar__item__count'>
+        <AnimatedNumber value={reaction.count} />
+      </span>
+    </animated.button>
+  );
 }
 
-
+export default StatusReactions
